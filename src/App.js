bb08bb32b47506cca277a4d7eb08e2eafa1571c0
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-08-04 14:33:34 +0300

    my header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+ const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="app-wrapper">
+      <Header className='header'>
+        <img src="https://img.freepik.com/free-vector/pelican-illustration-logo-design_343694-3232.jpg?t=st=1722766376~exp=1722769976~hmac=a2813496383544962493100f48e385f733abe48065f8f545c6c287feb8057248&w=1380" alt="logo"/>
+      />
+      <nav className='nav'>
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src="https://avatars.mds.yandex.net/i?id=d799ba7296bf38a6cf3e9cd7726006a5049ff977-4211909-images-thumbs&n=13" alt=""/>
+      </div>
+    </div>);
 }
 
 export default App;
