7583cb053a1c408a4910f154ae38236c6e83ea84
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-08-07 13:24:39 +0300

    refactor20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react'
+
 import './App.css';
+import React from 'react';
 import Header from "./component/Header/Header";
 import Navbar from "./component/Navbar/Navbar";
 import Profile from "./component/Profile/Profile";
 import Dialogs from "./component/Dialogs/Dialogs";
+import News from "./component/Profile/News/News";
+import Music from "./component/Profile/Music/Music";
+import Settings from "./component/Profile/Settings/Settings";
+import { BrowserRouter, Routes, Route, NavLink, Link} from "react-router-dom";
 
 
 
- const App = () => {
-  return (
-      <div className="app-wrapper">
-          <Header />
-          <Navbar />
 
-          <div className="app-wrapper-content">
-              {/*<Profile />*/}
-              <Dialogs/>
-          </div>
-      </div>);
- }
+const App = (props) => {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+
+            <div className="app-wrapper-content">
+                <BrowserRouter>
+                    <Routes>
+
+                        <Route path='/profile/*' element={<Profile/>}/>
+                        <Route path='/dialogs/*' element={<Dialogs/>}/>
+                        <Route path='/news/*' element={<News/>}/>
+                        <Route path='/music/*' element={<Music/>}/>
+                        <Route path='/settings/*' element={<Settings/>}/>
+                    </Routes>
+                </BrowserRouter>
+
+
+                    {/*<Route path='/dialogs' element={<Dialogs state={props.state.dialogPage}/>}/>*/}
+                    {/*<Route path='/profile' element={<Profile state={props.state.profilePage}/>}/>*/}
+            </div>
+        </div>);
+}
 
 export default App;
+
+
+
---
+++ src/component/Profile/Settings/Settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './Settings.module.css'
+
+
+const Settings = (props) => {
+    return (
+        <div>
+gigi
+        </div>
+    )
+
+}
+export default Settings
